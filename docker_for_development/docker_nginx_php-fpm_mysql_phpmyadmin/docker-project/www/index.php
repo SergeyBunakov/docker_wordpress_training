0a49c1af2c20590d3cd383795e25e7498296b29b
SergeyBunakov <31849954+SergeyBunakov@users.noreply.github.com>
2024-04-20 13:12:50 +0300

    Add files via upload

---
+++ docker_for_development/docker_nginx_php-fpm_mysql_phpmyadmin/docker-project/www/index.php
@@ -0,0 +1,17 @@
+<?php
+
+#$user = 'root';
+#$pass = 'root';
+
+#$dsn = 'mysql:host=mysql2;dbname=information_schema;charset=utf8';
+#$pdo = new PDO($dsn, $user, $pass);
+
+#$stmt = $pdo->query('SELECT * FROM tables');
+#$row = $stmt->fetch();
+
+#print_r($row);
+
+echo '<h1>Hello Docker and MySQL</h1>';
+echo '<h2 style="color: green">We will use it when learning PHP</h2>';
+
+
